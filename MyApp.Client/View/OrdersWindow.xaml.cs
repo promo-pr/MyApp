13cbafdfe8aacbac9782d802100aa36d72875942
step <79278993666@ya.ru>
2019-09-27 14:10:20 +0400

    init

---
+++ MyApp.Client/View/OrdersWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MyApp.Client.View
+{
+    /// <summary>
+    /// Interaction logic for OrdersWindow.xaml
+    /// </summary>
+    public partial class OrdersWindow : Window
+    {
+        public OrdersWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
